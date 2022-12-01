7b0cea127bae60cae21ac288901b4d4fffe6aef7
J.F. Godoy <juanfe.godoy@gmail.com>
2022-12-01 19:06:32 +0100

    delete styled components

--- src/components/Label.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { colors, familyfonts } from '../theme'
-
-export const Label = styled.label`
-  color: ${colors.bg_primary};
-  font-family: ${familyfonts};
-  margin: 0.5em;
-  font-size: 20px;
-`
